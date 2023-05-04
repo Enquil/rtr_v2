7c0de82c4cd921dcc12017914c8f648266f8950e
Enquil <jim.e.olesen@gmail.com>
2023-05-04 13:27:26 +0000

    register Comment model in admin, add action to remove comments that breaks rules etc.

--- newssite/admin.py
+++ newssite/admin.py
@@ -1,9 +1,40 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = (
+        'author',
+        'title',
+        'slug',
+        'status',
+        'created_on',
+        'category'
+        )
+    search_fields = ('title', 'content')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = (
+                    'name',
+                    'id',
+                    'body',
+                    'post',
+                    'created_on',
+                    'approved',
+                    'parent_id'
+                    )
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['disable_comment']
+
+    def disable_comments(self, request, queryset):
+        queryset.update(approved=False)
