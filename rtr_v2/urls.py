cf4867edfa1a17ec8cbf446ee2297a5fe22221dc
Enquil <jim.e.olesen@gmail.com>
2023-05-04 14:26:29 +0000

    add newssite.urls to rtr_v2.urls

--- rtr_v2/urls.py
+++ rtr_v2/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('newssite.urls'), name='newssite_urls'),
+    path('', include('newssite.urls'), name='newssite_urls'),
     path('summernote/', include('django_summernote.urls')),
     # path('accounts/', include('allauth.urls')),
 ]
