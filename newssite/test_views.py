dc1408d9ef211980f4a8061322bfe638a1f7ea98
Enquil <jim.e.olesen@gmail.com>
2023-05-06 21:53:34 +0000

    add testing for post_detail

---
+++ newssite/test_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+
+# class Testviews(TestCase):
+
+#     def test_get_post_detail(self):
+
+#         user = User(username='bob', is_superuser=True, password='bobspassword')
+#         post = Post(title='bobs post',
+#                     author=user,
+#                     slug='test-post')
+#         response = self.client.get('/test-post')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'post_detail.html')
--- post_detail/test_views.py
+++ post_detail/test_views.py
@@ -1,16 +1,24 @@
 from django.test import TestCase
 from newssite.models import Post
 from django.contrib.auth.models import User
+from django.shortcuts import (render, get_object_or_404,
+                              redirect, reverse)
+from django.http import (HttpResponse,
+                         HttpResponseRedirect)
+import requests
 
 
 class Testviews(TestCase):
 
     def test_get_post_detail(self):
-
-        user = User(username='bob', is_superuser=True, password='bobspassword')
-        post = Post.objects.create(title='bobs post',
-                                   author=user,
-                                   )
-        response = self.client.get('post_detail')
+        user = User(username='bob',
+                    is_superuser=True,
+                    password='bobspassword')
+        test = Post(title='bobspost',
+                    author=user,
+                    content="imbob",
+                    category="bobcat",
+                    slug='bobisaslug')
+        response = self.client.get(render(requests, template_name='post_detail.html'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'rtr_v2/post_detail.html')
+        self.assertTemplateUsed(response, 'post_detail.html')
