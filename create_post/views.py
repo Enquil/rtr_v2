58318ba50e49977bd5ac732258e557adb0185709
Enquil <jim.e.olesen@gmail.com>
2023-05-07 19:48:02 +0000

    create view for post creation

--- create_post/views.py
+++ create_post/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from newssite.models import Post
 
-# Create your views here.
+
+class CreatePost(View):
+
+    def get(self, request, *args, **kwargs):
+
+        return render(
+            request,
+            "create_post.html",
+            {
+                "post_form": PostForm()
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+
+        if request.method == "POST":
+            post_form = PostForm(request.POST, request.FILES)
+
+            if post_form.is_valid():
+                user = User.objects.get(id=request.user.id)
+                post_form.instance.email = request.user.email
+                post_form.instance.author = request.user
+                post = post_form.save(commit=False)
+                post.save()
+                return HttpResponseRedirect(
+                                            reverse(
+                                                    'post_detail',
+                                                    args=[post.slug]
+                                            ))
+            else:
+                post_form = PostForm()
