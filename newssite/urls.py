f8785c67f3b6aadbbd7f592f77204e0c907b72f3
Enquil <jim.e.olesen@gmail.com>
2023-05-08 19:13:32 +0000

    moved index.html to newssite and changed views to match new dir

--- newssite/urls.py
+++ newssite/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="index"),
-    # path('create_post/', views.CreatePost.as_view(), name='create_post'),
-    # path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('', views.PostList.as_view(), name="index"),
 ]
--- newssite/views.py
+++ newssite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 from django.views import generic
 from .models import Post
 
@@ -9,5 +10,5 @@ class PostList(generic.ListView):
     '''
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    template_name = "newssite/index.html"
     paginate_by = 10
