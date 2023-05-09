5051cfb8e0723d6a21bb3e68a665afd30b265aac
Enquil <jim.e.olesen@gmail.com>
2023-05-09 16:27:44 +0000

    added explanatory comments to tests

--- post_detail/test_views.py
+++ post_detail/test_views.py
@@ -10,50 +10,55 @@ import requests
 
 class TestPostDetailView(TestCase):
 
+    # Test templates when getting the PostDetail view
     def test_get_post_detail_templates(self):
-        user = User.objects.create(username='bob',
+        user = User.objects.create(username='alan',
                                    is_superuser=True,
-                                   password='bobspassword')
-        post = Post.objects.create(title='bobs post',
+                                   password='enigma')
+        post = Post.objects.create(title='how to crack codes',
                                    author=user,
-                                   content="hi, im bob",
+                                   content="so easy",
                                    category="general",
-                                   slug='bobs-post')
+                                   slug='how-to-crack-codes')
         post.likes.set(('1'))
         response = self.client.get(reverse('post_detail', args=(post.slug,)))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_detail/post_detail.html')
 
     def test_likes(self):
-        user = User.objects.create(username='bob',
+        user = User.objects.create(username='alan',
                                    is_superuser=True,
-                                   password='bobspassword')
-        post = Post.objects.create(title='bobs post',
+                                   password='enigma')
+        post = Post.objects.create(title='how to crack codes',
                                    author=user,
-                                   content="hi, im bob",
+                                   content="so easy",
                                    category="general",
-                                   slug='bobs-post')
+                                   slug='how-to-crack-codes')
         post.likes.set((''))
-        self.assertTrue(post.likes.count() == 0)
+        # Add a like test
+        self.assertFalse(post.likes.count() > 0)
         post.likes.add(user)
         self.assertTrue(post.likes.count() == 1)
+        # Template test
         response = self.client.get(reverse('post_detail', args=(post.slug,)))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_detail/post_detail.html')
 
     def test_remove_likes(self):
-        user = User.objects.create(username='bob',
+        user = User.objects.create(username='alan',
                                    is_superuser=True,
-                                   password='bobspassword')
-        post = Post.objects.create(title='bobs post',
+                                   password='enigma')
+        post = Post.objects.create(title='how to crack codes',
                                    author=user,
-                                   content="hi, im bob",
+                                   content="so easy",
                                    category="general",
-                                   slug='bobs-post')
+                                   slug='how-to-crack-codes')
         post.likes.set(('1'))
-        self.assertTrue(post.likes.count() == 1)
+        # Remove a like test
+        self.assertFalse(post.likes.count() == 0)
         post.likes.remove('1')
         self.assertTrue(post.likes.count() == 0)
+        # Template test
         response = self.client.get(reverse('post_detail', args=(post.slug,)))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_detail/post_detail.html')
