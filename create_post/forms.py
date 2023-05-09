8ed466bff5fd06c7e7d7361bf5926459ce23c5c6
Enquil <jim.e.olesen@gmail.com>
2023-05-09 11:15:41 +0000

    remove formhelpet attributes

--- create_post/forms.py
+++ create_post/forms.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 from django import forms
 from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 from django.utils.translation import ugettext_lazy as _
-from crispy_forms.helper import FormHelper
 
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'category', 'excerpt', 'content', 'featured_image')
+        fields = ('title', 'category', 'excerpt',
+                  'content', 'featured_image')
         widgets = {
             'content': SummernoteWidget(
                        attrs={'summernote': {
@@ -21,12 +21,3 @@ class PostForm(forms.ModelForm):
         labels = {
             'featured_image': 'Select an image'
         }
-        # helper = FormHelper()
-        # helper.form_class = 'form-group'
-        # helper.layout = (
-        #     Field('title', css_class='fs-1'),
-        #     Field('text', rows="3", css_class='form-control mb-3'),
-        #     Field('author', css_class='form-control mb-3'),
-        #     Field('tags', css_class='form-control mb-3'),
-        #     Field('slug', css_class='form-control'),
-        # )
