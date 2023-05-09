d02ca4df9bda8b289dcd7614c2bc5f730536d744
Enquil <jim.e.olesen@gmail.com>
2023-05-09 16:12:38 +0000

    added tests for adding and removing likes to a post

--- post_detail/test_views.py
+++ post_detail/test_views.py
@@ -8,9 +8,9 @@ from django.http import (HttpResponse,
 import requests
 
 
-class TestPostDetail(TestCase):
+class TestPostDetailView(TestCase):
 
-    def test_get_post_detail(self):
+    def test_get_post_detail_templates(self):
         user = User.objects.create(username='bob',
                                    is_superuser=True,
                                    password='bobspassword')
@@ -20,6 +20,40 @@ class TestPostDetail(TestCase):
                                    category="general",
                                    slug='bobs-post')
         post.likes.set(('1'))
+        response = self.client.get(reverse('post_detail', args=(post.slug,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post_detail/post_detail.html')
+
+    def test_likes(self):
+        user = User.objects.create(username='bob',
+                                   is_superuser=True,
+                                   password='bobspassword')
+        post = Post.objects.create(title='bobs post',
+                                   author=user,
+                                   content="hi, im bob",
+                                   category="general",
+                                   slug='bobs-post')
+        post.likes.set((''))
+        self.assertTrue(post.likes.count() == 0)
+        post.likes.add(user)
+        self.assertTrue(post.likes.count() == 1)
+        response = self.client.get(reverse('post_detail', args=(post.slug,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post_detail/post_detail.html')
+
+    def test_remove_likes(self):
+        user = User.objects.create(username='bob',
+                                   is_superuser=True,
+                                   password='bobspassword')
+        post = Post.objects.create(title='bobs post',
+                                   author=user,
+                                   content="hi, im bob",
+                                   category="general",
+                                   slug='bobs-post')
+        post.likes.set(('1'))
+        self.assertTrue(post.likes.count() == 1)
+        post.likes.remove('1')
+        self.assertTrue(post.likes.count() == 0)
         response = self.client.get(reverse('post_detail', args=(post.slug,)))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_detail/post_detail.html')
