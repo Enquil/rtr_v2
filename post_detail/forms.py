865571b8cc30a245acf87d5d622c47a72c862a09
Enquil <jim.e.olesen@gmail.com>
2023-05-11 10:14:30 +0000

    changed sizes of comment textarea, turned label into block element and same for submit button

--- post_detail/forms.py
+++ post_detail/forms.py
@@ -6,3 +6,5 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        labels = {'body': 'Leave a comment'}
+        widgets = {'body': forms.Textarea(attrs={'cols': 60, 'rows': 4})}
